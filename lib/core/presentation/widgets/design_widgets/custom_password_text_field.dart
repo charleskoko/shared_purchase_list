97011742a8140c4016b91d161665f3f364bd6905
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2022-01-01 21:23:57 +0100

    feature-1: Update custom_password_text_field

--- lib/core/presentation/widgets/design_widgets/custom_password_text_field.dart
+++ lib/core/presentation/widgets/design_widgets/custom_password_text_field.dart
@@ -2,33 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:shared_purchase_list/core/shared/colors.dart';
 
-class CustomPasswordTextField extends StatelessWidget {
+class CustomPasswordTextField extends StatefulWidget {
   final String hintext, label;
   final void Function(String? value) stringValueSetter;
   const CustomPasswordTextField({
     Key? key,
-    required this.stringValueSetter,
     required this.hintext,
     required this.label,
+    required this.stringValueSetter,
   }) : super(key: key);
 
   @override
+  _CustomPasswordTextFieldState createState() =>
+      _CustomPasswordTextFieldState();
+}
+
+class _CustomPasswordTextFieldState extends State<CustomPasswordTextField> {
+  bool obscureText = true;
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      obscureText: true,
+      obscureText: obscureText,
       style: TextStyle(
         fontSize: 14,
         color: kBleuColor,
         fontWeight: FontWeight.bold,
       ),
       decoration: InputDecoration(
-        hintText: hintext,
+        hintText: widget.hintext,
         hintStyle: TextStyle(
           fontSize: 14,
           color: kBleuColor,
           fontWeight: FontWeight.bold,
         ),
-        labelText: label,
+        labelText: widget.label,
         labelStyle: TextStyle(fontSize: 14, color: kBleuColor),
         enabledBorder: const UnderlineInputBorder(
           borderSide: BorderSide(
@@ -54,8 +61,24 @@ class CustomPasswordTextField extends StatelessWidget {
             color: kBleuColor,
           ),
         ),
+        suffixIcon: GestureDetector(
+          onTap: () {
+            setState(() {
+              obscureText ? obscureText = false : obscureText = true;
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+            ),
+            child: Icon(
+              obscureText ? Ionicons.eye_off_outline : Ionicons.eye_outline,
+              color: kBleuColor,
+            ),
+          ),
+        ),
       ),
-      onSaved: (value) => stringValueSetter(value),
+      onSaved: (value) => widget.stringValueSetter(value),
       validator: (value) {},
     );
   }

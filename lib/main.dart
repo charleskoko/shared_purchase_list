0da1119fccadb6e80c89621688c4cae7cad5c4d7
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2022-01-01 20:25:53 +0100

    feature-1: firebase setting up

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_purchase_list/core/presentation/widgets/app_router.dart';
 import 'package:shared_purchase_list/core/shared/service_locator.dart';
 import 'core/presentation/app_widget.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await setUpServiceLocator();
   runApp(MyApp(
     appRouter: AppRouter(),
